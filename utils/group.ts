57f4b50216642740b80e2c05f0dafb6d556b3792
Simo Nurmi <simo.aleksi.nurmi@gmail.com>
2022-06-14 23:26:29 +0300

    Add group utility function for grouping arrays
    Equal to group or groupBy proposal

---
+++ utils/group.ts
@@ -0,0 +1,7 @@
+const group = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
+  arr.reduce((groups, item) => {
+    (groups[key(item)] ||= []).push(item);
+    return groups;
+  }, {} as Record<K, T[]>);
+
+export default group;
